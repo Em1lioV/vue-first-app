18c4bb0b7a260bb2543dc5040d032e46d3d13148
Em1lioV <emilio.villagrawt@gmail.com>
2023-09-08 19:48:18 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>welcome...</p>
+  <teleport to=".modals" v-if="showModal">
+    <Modal theme="sale" @close="toggleModal">
+      <template v-slot:links>
+          <a href="">sign up now</a>
+          <a href="">more info</a>
+      </template>
+      <h1>Sign up from more kities</h1>
+      <p>grab your kitie and pray for the price</p>
+    </Modal>
+    
+  </teleport>
+  <div v-if="showModal2">
+    <Modal theme="" @close="toggleModal2">
+      <template v-slot:links>
+          <a href="">sign up now</a>
+          <a href="">more info</a>
+      </template>
+      <h1>Sign up from the newsletter</h1>
+      <p>for updates and promosssss!</p>
+    </Modal>
+    
+  </div>
+  <button @click="toggleModal" class="btn">modal teleport</button>
+  <button @click="toggleModal2" class="btn">open modal</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import { Teleport } from 'vue'
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Modal, Teleport },
+  data() {
+    return {
+      title: 'my first vue app',
+      header: 'Sign up from more kities',
+      text:'grab your kitie and pray for the price',
+      showModal:false,
+      showModal2:false
+    }
+  },
+  methods: {
+    HandleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+      this.$refs.name.focus()
+      
+    },
+    toggleModal(){
+      this.showModal = !this.showModal
+    },
+    toggleModal2(){
+      this.showModal2 = !this.showModal2
+    }
+  },
 }
 </script>
 
 <style>
-#app {
+#app, .modals{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn{
+  padding: 15px 7px;
+  margin: 10px;
+  border: 1px solid black;
+  background-color: #2c3e50;
+  color: aliceblue;
+  border-radius: 16px;
 }
 </style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="backdrop" @click.self="closeModal">
+        <div class="modal" :class="{ sale: theme === 'sale'}">
+
+            <slot>defalut content</slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        props:['header','text','theme'],
+        methods: {
+            closeModal(){
+                this.$emit('close')
+            }
+        },
+    }
+</script>
+<!-- 
+{scoped} tag hace que los estilos solo funcionen en este componenete - no se recomienda
+hacerlo mucho porque puede agregar carga extra 
+<style scoped>
+    estilos solo para el componente
+<style>
+-->
+<style> 
+    .modal{
+        width: 400px;
+        padding: 20px;
+        margin: 100px auto;
+        background: white;
+        border-radius:10px ;
+    }
+    .backdrop{
+        top: 0;
+        position: fixed;
+        background: rgba(0,0,0,0.5);
+        width: 100%;
+        height: 100%;
+    }
+    .modal h1{
+        color: #03cfb4;
+        border: none;
+        padding: 0;
+
+    }
+    .modal .actions{
+        text-align: center;
+        margin: 30px 0 10px 0;
+    }
+    .modal .actions a{
+        padding: 5px 10px;
+        text-decoration: none;
+        margin: 0 15px ;
+        color: #2c3e50;
+    }
+    .modal.sale{
+        background: crimson;
+        color: white;
+
+    }
+    .modal.sale h1{
+        color: white;
+        
+    }
+
+    .modal.sale .actions a{
+        border: 1px solid #ddd;
+        color: antiquewhite;
+
+     
+    }
+</style>
